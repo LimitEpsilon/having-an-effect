5e58f7259cf0560251ac777564bab16ec73a28d9
Joonhyup Lee <joonhyup@mit.edu>
2024-10-11 23:05:21 -0400

    Parameterize more

--- bin/main.ml
+++ bin/main.ml
@@ -45,16 +45,14 @@ let add_warp =
 let () =
   debug.set true;
   let open Domains in
-  let a_base = 0 in
-  let b_base = 200 in
   let mem iter_num : (Addr.t * int) list =
     let rec go acc n =
       if n <= 0 then acc
       else
         let acc =
           Addr.(
-            (of_int Int.(a_base + n - 1), n)
-            :: (of_int Int.(b_base + n - 1), Int.(iter_num + n))
+            (of_int Int.(n - 1), n)
+            :: (of_int Int.((2 * iter_num) + n - 1), Int.(iter_num + n))
             :: acc)
         in
         go acc (n - 1)
@@ -63,14 +61,14 @@ let () =
   in
   let imem_base = 0 in
   (* PRE : (R2) = loop iter *)
-  let loop : (Addr.t * inst) list =
+  let loop iter_num : (Addr.t * inst) list =
     Addr.
       [
         (of_int Int.(imem_base + 0), Ld (R3, of_int 0, R1));
         (* R3 ← M[(R1)] *)
         (of_int Int.(imem_base + 1), Add (R3, R3, R3));
         (* R3 ← (R3) + (R3) *)
-        (of_int Int.(imem_base + 2), St (of_int 100, R1, R3));
+        (of_int Int.(imem_base + 2), St (of_int iter_num, R1, R3));
         (* M[(R1) + 100] ← (R3) *)
         (of_int Int.(imem_base + 3), Addi (R1, R1, 1));
         (* R1 ← (R1) + 1 *)
@@ -81,7 +79,6 @@ let () =
         (of_int Int.(imem_base + 6), Halt);
       ]
   in
-  let prog = loop in
   let thread base iter_num =
     let arch = Exec [ Initial Gpu.cycle ] in
     let arch = Arch [ add_reg PC Addr.(of_int imem_base) arch ] in
@@ -92,9 +89,9 @@ let () =
     arch
   in
   let arch iter_num =
-    let arch = Arch [ thread a_base iter_num; thread b_base iter_num ] in
+    let arch = Arch [ thread 0 iter_num; thread (2 * iter_num) iter_num ] in
     let arch = Arch [ add_warp arch ] in
-    let arch = Arch [ add_mem Imem prog arch ] in
+    let arch = Arch [ add_mem Imem (loop iter_num) arch ] in
     let arch = add_mem Dmem (mem iter_num) arch in
     arch
   in
